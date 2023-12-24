057b039edaa266fa20ec24d807363cd4582d3ec9
penrodlol <christian@penrod.contact>
2023-12-24 17:04:56 -0500

    ✨ Table

---
+++ src/components/ui/table/index.ts
@@ -0,0 +1,9 @@
+// @ts-nocheck
+export { default as Body } from './components/body.astro';
+export { default as Caption } from './components/caption.astro';
+export { default as Cell } from './components/cell.astro';
+export { default as Footer } from './components/footer.astro';
+export { default as Head } from './components/head.astro';
+export { default as Header } from './components/header.astro';
+export { default as Root } from './components/root.astro';
+export { default as Row } from './components/row.astro';
