c5480734e2b84fc702bf5193e1e777641a54eb69
penrodlol <christian@penrod.contact>
2024-01-27 20:30:00 -0500

    ✨ Finish up toast (for now)

--- src/components/ui/toast/script.ts
+++ src/components/ui/toast/script.ts
@@ -1,12 +1,26 @@
 import { onAstroPageLoad } from '@/libs/astro';
 
-onAstroPageLoad<HTMLDivElement>('[data-toast]', (toast) => {
+type Toast = HTMLDivElement;
+
+let autoClose!: NodeJS.Timeout;
+
+onAstroPageLoad<Toast>('[data-toast]', (toast) => {
+  const duration = Number(toast.dataset.duration);
   const openTrigger = toast.nextElementSibling as HTMLButtonElement;
   const closeTriggers = toast.querySelectorAll<HTMLButtonElement>('[slot="toast-close"]');
 
-  openTrigger?.addEventListener('click', () => toast.setAttribute('data-state', 'open'));
+  openTrigger?.addEventListener('click', () => {
+    document.querySelectorAll<Toast>('[data-toast]').forEach((_toast) => close(_toast, autoClose));
+    toast.setAttribute('data-state', 'open');
+    if (duration !== Infinity) autoClose = setTimeout(() => close(toast), duration);
+  });
 
   closeTriggers?.forEach((closeTrigger) => {
-    closeTrigger?.addEventListener('click', () => toast.setAttribute('data-state', 'closed'));
+    closeTrigger?.addEventListener('click', () => close(toast, autoClose));
   });
 });
+
+function close(toast: Toast, autoClose?: NodeJS.Timeout) {
+  toast.setAttribute('data-state', 'closed');
+  if (autoClose) clearTimeout(autoClose);
+}
