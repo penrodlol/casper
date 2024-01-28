c13c7ddf3c54b3bc7e0cfcdada973aaaf83dd627
penrodlol <christian@penrod.contact>
2024-01-27 19:45:46 -0500

    ✨ Form

---
+++ src/components/ui/form/index.ts
@@ -0,0 +1,7 @@
+// @ts-nocheck
+export { default as Description } from './components/description.astro';
+export { default as Field } from './components/field.astro';
+export { default as Fieldset } from './components/fieldset.astro';
+export { default as Label } from './components/label.astro';
+export { default as Legend } from './components/legend.astro';
+export { default as Root } from './components/root.astro';
---
+++ src/components/ui/form/script.ts
@@ -0,0 +1,22 @@
+import { onAstroPageLoad } from '@/libs/astro';
+
+const controls = 'input,textarea,select,button[role="combobox"]';
+
+onAstroPageLoad<HTMLDivElement>('[data-form]', (form) => {
+  form.querySelectorAll<HTMLFieldSetElement>('fieldset').forEach((fieldset) => {
+    const legend = fieldset.querySelector<HTMLLegendElement>('legend');
+    if (legend) fieldset.setAttribute('aria-labelledby', legend.id);
+  });
+
+  form.querySelectorAll<HTMLDivElement>('[data-field]').forEach((field) => {
+    const label = field.querySelector<HTMLLabelElement>('label');
+    const control = field.querySelector<HTMLElement>(controls);
+    const description = field.querySelector<HTMLDivElement>('[data-description]');
+    if (!label || !control) return;
+
+    label.setAttribute('for', `${form.dataset.form}-${field.dataset.name}`);
+    control.setAttribute('id', `${form.dataset.form}-${field.dataset.name}`);
+    control.setAttribute('aria-labelledby', label.id);
+    if (description) control.setAttribute('aria-describedby', description.id);
+  });
+});
